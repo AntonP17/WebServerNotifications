88c711fb8d762707ccb11ed8e21ef4c9695fb1d1
AntonP17 <pokrovskii.anton@mail.ru>
2025-05-28 23:33:40 +0300

    finish

--- src/main/resources/migration/changelog/v10__migration_add_new_services.sql
+++ src/main/resources/migration/changelog/v10__migration_add_new_services.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 --changeset antoxakon:4  -- Формат: author:id
 --comment: add new 2 services!
-
-insert into services (id, name) VALUES (2,'netflix'),
+insert into services (id, name) VALUES (1,'kinopoisk'),
+                                         (2,'netflix'),
                                        (3,'vk_music');
